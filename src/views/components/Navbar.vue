1218c8bc22546b3ea37b3ebe2068bca4a804e10f
Andy <admazzola@gmail.com>
2021-01-27 11:36:55 -0500

    frontend

--- src/views/components/Navbar.vue
+++ src/views/components/Navbar.vue
@@ -13,7 +13,7 @@
        </div>
 
 
-       <div class="inline-block lg:hidden  pull-right p-4">
+       <div class="inline-block lg:hidden  pull-right p-4" style="float:right">
 
          <button @click="showResponsiveMenu=!showResponsiveMenu" class="flex items-right px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
