ac334a657d4cfd1f2e82e9c8b802ece9a2acdb24
andy <admazzola@gmail.com>
2021-01-30 23:29:44 -0500

    navbar

--- src/views/config/UpperNav.js
+++ src/views/config/UpperNav.js
@@ -2,9 +2,8 @@ const config={
 
   "dropdowns":[
     {"title":"DOCS","rows":[
-     
-      {"title":"White Paper","url":"https://github.com/0xbitcoin/white-paper"},
-      {"title":"Smart Contract","url":"https://etherscan.io/address/0xb6ed7644c69416d67b522e20bc294a9a9b405b31#code"},
+      {"title":"F.M.T. Github","url":"https://github.com/mineable-tokens-farm"},
+      {"title":"Zapper.Fi Docs","url":"https://docs.zapper.fi/build/smart-contracts"},
       {"title":"0xBTC Explorer","url":"https://0xbtc.info/"}
     ]},
     {"title":"EXCHANGES","rows":[
@@ -18,9 +17,8 @@ const config={
     {"title":"COMMUNITY","rows":[
       {"title":"Discord","url":"https://discordapp.com/invite/JGEqqmS"},
       {"title":"Reddit","url":"https://www.reddit.com/r/0xbitcoin/"},
-      {"title":"Forums","url":"https://forums.0xbtc.io/"},
-      {"title":"Github","url":"https://github.com/0xbitcoin"},
-      {"title":"Gitter","url":"https://gitter.im/0xbtc-official/development"}
+      {"title":"Forums","url":"https://forums.0xbtc.io/"}
+
     ]}
   ]
 
